40539f45e419a21eadbba84bf63cd00367f2804d
zhoracementov <zoracementov92551@gmail.com>
2025-02-13 12:46:24 +0300

    back - fix description info lost

--- backend-java-spring/src/main/java/com/example/testproject/repository/DocumentDTO.java
+++ backend-java-spring/src/main/java/com/example/testproject/repository/DocumentDTO.java
@@ -5,11 +5,13 @@ import java.util.List;
 public class DocumentDTO {
     private Long id;
     private String fileName;
+    private String description;
     private List<String> tags;
 
     public DocumentDTO(Document document) {
         this.id = document.getId();
         this.fileName = document.getFileName();
+        this.description = document.getDescription();
         this.tags = document.getTags().stream().map(Tag::getName).toList();
     }
 
@@ -27,6 +29,14 @@ public class DocumentDTO {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public List<String> getTags() {
